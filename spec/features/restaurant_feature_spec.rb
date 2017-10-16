b93b5fb034befa3c9b856f2155e478f0b16d7813
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-16 22:00:06 +0100

    Add second feature test for restaurants

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -8,4 +8,13 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurant has been added' do
+    scenario 'should display restaurant' do
+      Restaurant.create(name: "Pizza Express")
+      visit '/restaurants'
+      expect(page).to have_content("Pizza Express")
+      expect(page).not_to have_content("No restaurants yet")
+    end
+  end
 end
