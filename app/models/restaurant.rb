d9e202c7b0c7463b2ad900dcccc2a24f47ef7e49
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-18 21:35:01 +0100

    Displays the average rating of each restaurant on the restaurant listings page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,16 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  belongs_to :user
+  # belongs_to :user
   validates :name, presence: true,
                    length: {minimum: 2}, uniqueness: true
+
+  def average_rating
+    restaurant_reviews = Review.where(restaurant_id: self.id)
+    if restaurant_reviews.length == 0
+      return "No ratings yet for this restaurant"
+    else
+      restaurant_reviews.average(:rating)
+    end
+  end
+
 end
